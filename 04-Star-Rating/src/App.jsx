496d899df7b9fcc107c8b30306ba23466e3de06b
Raees Mughal <raees.mughal.official@gmail.com>
2026-08-31 21:59:50 +0500

    image slider

--- 04-Star-Rating/src/App.jsx
+++ 04-Star-Rating/src/App.jsx
@@ -4,8 +4,22 @@ import './App.css'
 import { FaStar } from 'react-icons/fa'
 function App() {
 
-  const [totalStar, setTotalStar] = useState(null);
+  const [totalStar, setTotalStar] = useState(0);
+  const [rating,setRating] = useState(0);
+  const [hover,setHover] = useState(0);
 
+  function handleClick(currentIndex) {
+    setRating(currentIndex);
+  }
+
+  function handleMouseOver(currentIndex) {
+    setHover(currentIndex);
+  }
+  
+  function handleMouseLeave() {
+    setHover(rating)
+  }
+console.log(hover)
 
   function starsPrompt() {
     const p = prompt('out of how many stars you want to rate (total no. of stars)?');
@@ -13,10 +27,9 @@ function App() {
     if (!isNaN(p) && p > 0) {
       console.log('total start will be ' + p);
       setTotalStar(Number(p));
-    }else{
+    } else {
       console.log('please enter a valid number')
     }
-
   }
 
   return (
@@ -25,11 +38,18 @@ function App() {
       <button type="button" onClick={starsPrompt}>Give your feedback</button>
 
       {
-        
+        [...Array(totalStar)].map((_, index) => {
+          index += 1
+          return <FaStar
+            key={index}
+            size={40}
+            onClick={() => handleClick(index)}
+            onMouseOver={()=>handleMouseOver(index)}
+            onMouseLeave={handleMouseLeave}
+            className = {index <= (hover || rating) ? 'a':'b'}
+          />
+        })
       }
-
-
-
 
     </div>
   )
---
+++ 05-Image-Slider/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="image-slider">
+      Image Slider
+    </div>
+  )
+}
+
+export default App
