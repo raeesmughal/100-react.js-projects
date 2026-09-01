acb7cf27ceb81f9f7b7a52e363ebc787f983e268
Raees Mughal <raees.mughal.official@gmail.com>
2026-09-01 19:23:19 +0500

    (image slider + load more button) finished

--- 05-Image-Slider/src/App.jsx
+++ 05-Image-Slider/src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import ImageSlider from './components/ImageSlider'
 
 function App() {
 
   return (
-    <div className="image-slider">
-      Image Slider
+    <div className='app-container'>
+      <ImageSlider url={"https://picsum.photos/v2/list"} limit={13} page={43} />
     </div>
   )
 }
