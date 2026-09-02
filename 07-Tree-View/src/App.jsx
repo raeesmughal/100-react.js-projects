638e33966a445049a90b3f4cc17a455637a03bc7
Raees Mughal <raees.mughal.official@gmail.com>
2026-09-02 23:16:17 +0500

    tree view project initialized

---
+++ 07-Tree-View/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>Hello Tree View</div>
+  )
+}
+
+export default App
---
+++ 08-Light-Dark-Mode/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam praesentium commodi quaerat blanditiis est obcaecati, nihil autem libero aliquam quibusdam? Similique eligendi doloribus natus alias dolorem veniam dolore sit laboriosam.
+   </div>
+  )
+}
+
+export default App
